f38c6d33b3f557dcaf55fa3fd03436e7fddd48a7
Rus Andreea <aramonarus1@yahoo.com>
2022-03-04 17:41:23 +0200

    Add project files.

---
+++ MPP_MusicFestival/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MPP_MusicFestival
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
